fa2046299cf2bd8f32673ed876a8290743debcad
Alexander Mahler <asmahler@gmail.comssh-keygen -t rsa -b 4096 -C asmahler@gmail.com>
2016-05-16 17:11:00 -0700

    made some changes

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__) 
+
+
 
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/show',methods=['GET','POST'])
+def show(): 
+	name = request.form['name']
+	if request.method == "POST":
+		return render_template('show.html',name=name)
+	else: 
+		return render_template('spoilers.html')
 
 
 
---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+console.log('test');
+
+
+function background(){
+var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'];
+var background = document.querySelector('body');
+var random = Math.floor(Math.random() * 5);
+	background.style.backgroundImage = "url(/static/images/"+images[random]+")";
+		
+	}
+
+window.onload = function(){
+	background();
+}
